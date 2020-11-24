2ac6a45e51c5de1e68f9c293141c9cbaf7d59fd5
Robert Pollack <rpollack9974@gmail.com>
2020-11-24 13:36:53 -0500

    changed format of file name

--- low_slopes.py
+++ low_slopes.py
@@ -1,6 +1,6 @@
 def write_low_slopes_to_file(p,N,num_coefs,min_weight,max_weight):
 	g = ghost(p,N,num_coefs=num_coefs)
-	filename = str(p) + "." + str(N) + "." + "low_slopes." + str(num_coefs)
+	filename = "DATA/low_slopes." + str(p) + "." + str(N) + "." + str(num_coefs) 
 	for k in range(min_weight,max_weight,2):
 		f = open(filename,"a")
 		slopes = g.slopes(k)
